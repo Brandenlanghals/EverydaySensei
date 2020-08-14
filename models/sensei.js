2425aa70e53d6943c96b67df566f55d111b9b855
Branden Langhals <BrandenLanghals@gmail.com>
2020-08-13 21:02:56 -0400

    added images folder

--- models/sensei.js
+++ models/sensei.js
@@ -6,7 +6,6 @@ var Sequelize = require("sequelize");
 var sequelize = require("../config/connection.js");
 // Require the connection to the database (connection.js)
 
-<<<<<<< HEAD
     var Sensei = sequelize.define("Sensei", {
       name: {
         type: Sequelize.STRING
@@ -54,45 +53,3 @@ var sequelize = require("../config/connection.js");
   
     module.exports = Sensei;
  
-=======
-var Sensei = sequelize.define("Sensei", {
-  name: {
-    type: Sequelize.STRING,
-  },
-  geo_coordinates: {
-    type: Sequelize.STRING,
-  },
-  email: {
-    type: Sequelize.STRING,
-  },
-  bio: {
-    type: Sequelize.STRING,
-  },
-  skill_level: {
-    type: Sequelize.INTEGER,
-  },
-  credits: {
-    type: Sequelize.INTEGER,
-  },
-  rating: {
-    type: Sequelize.INTEGER,
-  },
-  profile_photo: {
-    type: Sequelize.STRING.BINARY,
-  },
-  interests: {
-    type: Sequelize.STRING,
-  },
-  skill_id: {
-    type: Sequelize.INTEGER,
-  },
-});
-
-Sensei.sync();
-//Associating Sensei with Student
-Sensei.associate = function (models) {
-  Sensei.hasMany(models.Student, { through: models.SenseiStudentJoin });
-};
-
-module.exports = Sensei;
->>>>>>> 10d05aaa0e71e77ccd1033d1dfd2d856332756f8
